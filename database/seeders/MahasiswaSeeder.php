e8ccbee308829afa0f21d87a9bd4fbfcb108d6fe
aliefdany <aliefseventri@gmail.com>
2024-03-27 20:14:42 +0700

    added mahasiswa seeder

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'nama' => "Alief Dany Seventri",
+            'nim' => '195150400111002',
+            'alamat' => 'Karangploso, Malang, Jawa Timur',
+            'hobi' => "Badminton"
+        ]);
+    }
+}
